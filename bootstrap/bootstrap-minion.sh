2919ec037e1748888d3c877045d10a00d17386d0
Felix Möller <git@felix-moeller.net>
2016-05-06 22:12:30 +0200

    Update bootstrap script

--- bootstrap/bootstrap-minion.sh
+++ bootstrap/bootstrap-minion.sh
@@ -21,8 +21,8 @@ info="[ ${cyan}INFO${nc} ]"
 
 # Make sure script is run as root
 if [[ $EUID -ne 0 ]]; then
-	echo "Please run this script as root" 1>&2
-	exit 1
+    echo "Please run this script as root" 1>&2
+    exit 1
 fi
 
 
@@ -37,55 +37,67 @@ echo
 declare -a pkg_req=(salt-minion python-pip git)
 
 # echo -n "Updating repositories... "
-# [[ $(apt-get update 1>/dev/null 2> >(wc -l)) -gt 0 ]] && (echo -e "\n  ${err} Error updating repositories..." 1>&2; exit 3)
-# echo -e "\n"
+## REMOVE COMMENTS
+## [[ $(apt-get update 1>/dev/null 2> >(wc -l)) -gt 0 ]] && (echo -e "\n  ${err} Error updating repositories..." 1>&2; exit 3)
+## echo -e "\n"
 
-echo "Checking installed system packages..."
-for p in ${pkg_req[@]}; do
-	echo -n "  "
-	if [[ $(dpkg-query -W $p 2>/dev/null) ]]; then
-		echo -ne "${ok}"
-	else
-		echo -ne "${noy}"
-		pkg_new+=($p)
-	fi
-	echo " $p"
-done
-echo
-
-if [[ ${#pkg_new[@]} -gt 0 ]]; then
-	echo -n "Installing required system packages... "
-	apt-get install -y ${pkg_new[@]} &>/dev/null || (echo -e "\n  ${err} Error installing required packages" 1>&2; exit 4)
-	echo -e "\n"
-fi
+#echo "Checking installed system packages..."
+#for p in ${pkg_req[@]}; do
+#    echo -n "  "
+#    if [[ $(dpkg-query -W $p 2>/dev/null) ]]; then
+#        echo -ne "${ok}"
+#    else
+#        echo -ne "${noy}"
+#        pkg_new+=($p)
+#    fi
+#    echo " $p"
+#done
+#echo
+#
+#if [[ ${#pkg_new[@]} -gt 0 ]]; then
+#    echo -n "Installing required system packages... "
+#    if [[ !$(apt-get install -y ${pkg_new[@]} &>/dev/null) ]]; then
+#       echo -e "\n  ${err} Error installing required packages" 1>&2
+#       exit 4
+#    fi
+#    echo -e "\n"
+#fi
 
 
 # Check python modules
-declare -a py_req=(shyaml shyxaml)
+declare -a py_req=(shyaml)
 
-echo "Checking installed python modules..."
-for p in ${py_req[@]}; do
-	echo -n "  "
-	if [[ $(pip show ${p}) ]]; then
-		echo -ne "${ok}"
-	else
-		echo -ne "${noy}"
-		py_new+=($p)
-	fi
-	echo " $p"
-done
-echo
-
-if [[ ${#py_new[@]} -gt 0 ]]; then
-    echo "Installing required python modules... "
-    for n in ${py_new[@]}; do
-        [[ `pip search "$n"` ]] || (echo -e "  ${err} $n not found"; exit 5)
-        echo "Continueing nontheless..."
-        piplog=$(pip2 install "$n")
-        [[ $? -eq 0 ]] && echo -e "  ${ok} Success" || (echo -e "  ${err} Error"; echo "$piplog"; exit 6)
-	    echo
-    done
-fi
+#echo "Checking installed python modules..."
+#for p in ${py_req[@]}; do
+#    echo -n "  "
+#    if [[ $(pip show ${p}) ]]; then
+#        echo -ne "${ok}"
+#    else
+#        echo -ne "${noy}"
+#        py_new+=($p)
+#    fi
+#    echo " $p"
+#done
+#echo
+#
+#if [[ ${#py_new[@]} -gt 0 ]]; then
+#    echo "Installing required python modules... "
+#    for n in ${py_new[@]}; do
+#        if [[ $(pip search "$n" | wc -l) -lt 1 ]]; then
+#           echo -e "  ${err} $n not found"
+#           exit 5
+#        fi
+#        piplog=$(pip install "$n" &>1)
+#        if [[ $? -eq 0 ]]; then
+#            echo -e "  ${ok} Success"
+#        else
+#            echo -e "  ${err} Error"
+#            echo "${piplog}"
+#            exit 6
+#        fi
+#        echo
+#    done
+#fi
 
 
 # Use masterless local minion mode if not already set
@@ -94,15 +106,108 @@ echo "Configuring salt..."
 minion_file=/etc/salt/minion
 minion_local="file_client: local"
 grep "${minion_local}" ${minion_file} &>/dev/null && echo -e "  ${ok} Local minion mode already enabled" || ( sed -i "/#file_client: remote/a ${minion_local}" ${minion_file}; echo -e "  ${ok} Enabled local minion mode" )
+
+# Clone salt-raspi repo
+gh_user="flipdot"
+gh_repo="salt-raspi"
+cd /root
+if [[ -d ${gh_repo} ]]; then
+    echo -e "  ${ok} ${gh_repo} already cloned"
+else
+    git clone -q https://github.com/${gh_user}/${gh_repo}.git 2>/dev/null && echo -e "  ${ok} ${gh_repo} successfully cloned" || echo -e "  ${err} ${gh_repo} could not be cloned"
+    echo
+fi
+[[ -d /srv ]] && echo -e "  ${ok} /src already existent" || (mkdir /srv; echo -e "  ${ok} /src successfully created")
+[[ -L /srv/salt ]] && echo -e "  ${ok} /srv/salt already existent" || (ln -s "/root/${gh_repo}/state" /srv/salt; echo -e "  ${ok} /srv/salt successfully linked")
+[[ -L /srv/pillar ]] && echo -e "  ${ok} /srv/pillar already existent" || (ln -s "/root/${gh_repo}/pillar" /srv/pillar; echo -e "  ${ok} /srv/pillar successfully linked")
 echo
 
 
-# Clone salt-conf repo
-repo_name="salt-raspi"
-cd /root
-[[ -d ${repo_name} ]] || (echo "Getting 'Raspberry Pi' salt configuration via git..."; git clone -q https://github.com/flipdot/${repo_name}.git ; echo)
-[[ -d /srv ]] || mkdir /srv
-[[ -L /srv/salt ]] || ln -s "/root/$repo_name/state" /srv/salt
-[[ -L /srv/pillar ]] || ln -s "/root/$repo_name/pillar" /srv/pillar
+echo "Setting up minion..."
 
+minion_file="${gh_repo}/pillar/minions.sls"
+domain="fd"
+minion_root="minions"
+minion_list=($(cat "${minion_file}" | shyaml keys ${minion_root}))
 
+# TODO: unfuck this!?
+
+function choose_hostname() {
+    echo "  Choose available hostname:"
+
+    i=1
+    for l in ${minion_list[@]}; do
+        echo -e "   ${cyan}$i${nc}: $l"
+        ((i++))
+    done
+    echo
+
+    echo -ne "    Hostname: ${cyan}"
+    read minion_id
+    ((minion_id--))
+    echo -e "${nc}"
+
+    # Check if hostname is already in use
+
+    h="${minion_list[$minion_id]}.${domain}"
+    s="${h} has address "
+    ip_dns=$(host ${h} | grep "${s}" | sed "s/${s}//")
+    [[ $ip_dns = "" ]] && ip_dns="${red}NOT FOUND${nc}"
+    ips=($(hostname -I))
+
+    minion_yaml="${minion_pre}${minion_list[$minion_id]}.sls"
+    # shyaml get-value minions.tv\\.fd.ip
+
+    if [[ ${minion_id} =~ ^-?[0-9]+$ && ${minion_id} -lt ${#minion_list[@]} && $minion_id -ge 0 ]]; then
+        minion_name=${minion_list[${minion_id}]}
+        minion_name_escaped=$(echo "${minion_name}" | sed 's/\./\\./g')
+
+        # Check if the IP resolved via DNS is contained within the set of local IPs
+        #if [[ $(cat ${minion_file} | shyaml get-value ${minion_root}.${minion_name_escaped}) == "True" ]]; then
+        #  | shyaml get-value minions.tv\\.fd.ip
+
+        ip_salt=$(cat ${minion_file} | shyaml get-value ${minion_root}.${minion_name_escaped}.ip)
+        [[ $ip_salt = "" ]] && ip_salt="${red}NOT FOUND${nc}"
+
+        if [[ $ip_dns == *"${ips}"* && $ip_salt == *"${ips}"* ]]; then
+            #if [[ ! $(ping -W 2 -c1 ${minion_list[$minion_id]}.${domain} ) ]]; then
+            echo -e "    ${ok} ${minion_list[$minion_id]} chosen without conflicts"
+            return $minion_id
+        else
+            echo -e "    ${err} ${minion_list[$minion_id]} has the wrong IP address"
+            echo "              now:  ${ips}"
+            #for k in ${ips[@]}; do
+            #    echo $k
+            #done
+            echo -e "              dns:  ${ip_dns}"
+            echo -e "              salt: ${ip_salt}"
+            echo
+            echo -ne "              Choose anyways? (${green}y${nc}/${red}N${nc}) ${cyan}"
+            read override
+            echo -e "${nc}"
+
+            if [[ "$override" == "y" ]]; then
+                return $minion_id
+            fi
+            choose_hostname
+        fi
+    else
+        echo -e "    ${err} Your input was not valid\n"
+        choose_hostname
+    fi
+}
+
+choose_hostname
+hostname=${minion_list[$?]}
+
+# Change hostname in running session
+sysctl kernel.hostname="${hostname}" &>/dev/null
+# Make the change permanent
+echo ${hostname} > /etc/hostname
+
+# Salt's first run
+echo -e "\nSalt is taking over now...\n"
+salt-call state.highstate 2>/dev/null
+
+# Reload new hostname in newly opened shell
+exec ${SHELL}
